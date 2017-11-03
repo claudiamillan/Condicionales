c4455336ebbbf56814b2f41256c59da6fb722882
claudiamillan <millanc569@gmail.com>
2017-11-03 19:17:49 -0300

    Promedio de nota

---
+++ src/condicionales/Calificacion2.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lab12
+ */
+public class Calificacion2 
+{
+public static void main(String[] args)   
+{
+ float nota7, nota6, nota5, nota4; 
+ 
+nota7 = Float.parseFloat(JOptionPane.showInputDialog("ingresa la nota7"));
+nota6 = Float.parseFloat(JOptionPane.showInputDialog("ingrese la nota6"));
+nota5 = Float.parseFloat(JOptionPane.showInputDialog("ingrese la nota5"));
+nota4 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la nota4"));
+
+
+if (nota== notable, bien, suficiente, insuficiente)//Aqui va una condicion
+{
+    
+//Si la condicion se cumple se ejecuta este codigo 
+      JOptionPane.showMessageDialog(null, "notable + nota");
+      
+}
+      JOptionPane.showMessageDialog(null, "bien + nota");
+{      
+      JOptionPane.showMessageDialog(null, "suficiente+ nota");
+}      
+      JOptionPane.showMessageDialog(null, "insuficiente + nota");
+ 
+} 
+
+      else
+{
+     //Si no se cumple la condicion se ejecuta esto
+     JOptionPane.showMessageDialog(null, "nota7 = notable");
+}
+     
+{
+     JOptionPane.showMessageDialog(null, "nota6 = bien");
+}
+
+{     
+     JOptionPane.showMessageDialog(null, "nota5 = suficiente");
+}
+
+{     
+     JOptionPane.showMessageDialog(null, "nota4 = insuficiente");
+}        
+}
+}        
+}    
